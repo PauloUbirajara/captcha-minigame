1e1dcf6e0b8cec0d262750a077ba75d144d09546
PauloUbirajara <ubirajara3@hotmail.com>
2022-04-16 19:58:07 -0300

    Adicionar método para verificar token

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,7 +1,40 @@
+import axios from 'axios';
+
 class APIController {
 	get = (req, res) => {
 		console.log(req);
 		return res.json({ success: true });
+	};
+
+	verify = async (req, res) => {
+		const { token } = req.body;
+
+		if (!token) {
+			return res.status(400).json({
+				error: "Atributo 'token' não fornecido no corpo da requisição"
+			});
+		}
+
+		if (!process.env.SECRET_KEY) {
+			return res
+				.status(401)
+				.json({ error: "Variável de ambiente 'SECRET_KEY' não definida" });
+		}
+
+		const URL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`;
+
+		const result = await axios
+			.post(URL)
+			.then((data) => {
+				return res.status(200).json({ data });
+			})
+			.catch((err) => {
+				return res.status(401).json({
+					error: `Houve um erro durante a verificação com a API do ReCaptchaV3: ${err}`
+				});
+			});
+
+		return result;
 	};
 }
 
