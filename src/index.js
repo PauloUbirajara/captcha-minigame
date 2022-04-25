c0d48af63b5885c6fb007dcbf478eaca3f682f13
PauloUbirajara <ubirajara3@hotmail.com>
2022-04-25 13:45:06 -0300

    Instalar dependência CORS

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import apiRoutes from './routes/api.routes.js';
 
 dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(apiRoutes);
+
+// TODO - Criar token a partir da finalização de um minigame
+// TODO - Passar token do client pra API e retornar algo visual pro client
 
 const SERVER_PORT = process.env.PORT || 3000;
 
 const onServerOpen = () => {
 	console.log(`Disponivel em http://localhost:${SERVER_PORT}`);
 };
-
-const app = express();
-
-app.use(express.json())
-app.use(apiRoutes);
-
-// TODO - Criar token a partir da finalização de um minigame
-// TODO - Passar token do client pra API e retornar algo visual pro client
 
 app.listen(SERVER_PORT, onServerOpen);
