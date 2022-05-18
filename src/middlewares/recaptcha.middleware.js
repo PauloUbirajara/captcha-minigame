26b4ad188f41b3573ff492aeae3b4fa1b5e0f2a4
PauloUbirajara <ubirajara3@hotmail.com>
2022-05-18 17:59:37 -0300

    Unificar funções para verificar tokens e chaves relacionadas ao captcha

--- src/middlewares/recaptcha.middleware.js
+++ src/middlewares/recaptcha.middleware.js
@@ -9,3 +9,15 @@ export const checkForSecretKey = (_req, res, next) => {
 
 	return next();
 };
+
+export const checkForToken = (req, res, next) => {
+	const { token } = req.body;
+
+	if (!token) {
+		return res.status(400).json({
+			error: "Atributo 'token' não fornecido no corpo da requisição"
+		});
+	}
+
+	return next();
+};
